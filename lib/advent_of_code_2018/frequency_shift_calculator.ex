b1fc15af3d9590d2c5c36f28d02eb027ccd04bf7
John Wilger <john@johnwilger.com>
2018-12-01 14:09:00 -0800

    Get rid of string parsing

--- lib/advent_of_code_2018/frequency_shift_calculator.ex
+++ lib/advent_of_code_2018/frequency_shift_calculator.ex
@@ -2,36 +2,43 @@ defmodule AdventOfCode2018.FrequencyShiftCalculator do
   def calibrate([]), do: {:ok, 0}
 
   def calibrate(operations, reporter \\ fn _ -> nil end) do
-    if all_the_same_type?(operations) do
-      {:error, "frequency will never normalize"}
-    else
-      {:ok, perform_calibration(operations, reporter)}
-    end
+    operations
+    |> parse
+    |> check_for_never_ending_list
+    |> perform_calibration(reporter)
   end
 
   defp all_the_same_type?(operations) do
-    Enum.all?(operations, &is_addition?/1) ||
-      Enum.all?(operations, &is_subtraction?/1)
+    Enum.all?(operations, &is_positive?/1) ||
+      Enum.all?(operations, &is_negative?/1)
   end
 
-  defp is_addition?("+" <> _), do: true
-  defp is_addition?("-" <> _), do: false
-  defp is_addition?(operation), do: raise("Invalid frequency shift: #{operation}")
-  defp is_subtraction?(operation), do: !is_addition?(operation)
+  def is_positive?(n), do: n >= 0
+  def is_negative?(n), do: !is_positive?(n)
 
-  defp perform_calibration(operations, reporter) do
-    operations
-    |> parse
+  defp perform_calibration({:ok, operations}, reporter) do
+    result = operations
     |> Stream.cycle
     |> reduce_until_duplicate_found(reporter)
+    {:ok, result}
   end
+  defp perform_calibration(result, _reporter), do: result
 
   defp parse(operations) do
-    Enum.map(operations, fn operation ->
+    result = Enum.map(operations, fn operation ->
       operation
       |> String.trim
       |> String.to_integer
     end)
+    {:ok, result}
+  end
+
+  defp check_for_never_ending_list({:ok, operations}) do
+    if all_the_same_type?(operations) do
+      {:error, "frequency will never normalize"}
+    else
+      {:ok, operations}
+    end
   end
 
   defp reduce_until_duplicate_found(operations, reporter) do
