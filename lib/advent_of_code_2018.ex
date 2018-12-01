ff56f0687a93a3bf62c2476f67062568cc48f2a4
John Wilger <john@johnwilger.com>
2018-12-01 12:20:38 -0800

    add some progress output just for fun

--- lib/advent_of_code_2018.ex
+++ lib/advent_of_code_2018.ex
@@ -1,10 +1,10 @@
 defmodule AdventOfCode2018 do
   alias AdventOfCode2018.FrequencyShiftCalculator
 
-  def calibrate(frequency_shift_file) do
+  def calibrate(frequency_shift_file, reporter \\ fn _ -> nil end) do
     frequency_shift_file
     |> File.stream!
     |> Enum.into([])
-    |> FrequencyShiftCalculator.calibrate()
+    |> FrequencyShiftCalculator.calibrate(reporter)
   end
 end
